cbdfc4f99463dff98aa49e0c04f9bb48d2655a84
Przemek Malak <przemek.malak@gmail.com>
2022-10-19 20:11:07 +0200

    Working state machine

--- lib/step-function-retries-stack.ts
+++ lib/step-function-retries-stack.ts
@@ -3,16 +3,13 @@ import { Construct } from 'constructs';
 import * as sf from 'aws-cdk-lib/aws-stepfunctions';
 import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Lambda } from 'aws-sdk';
-import { IFunction } from 'aws-cdk-lib/aws-lambda';
-
 
 export interface StepFunctionRetriesStackProps extends cdk.StackProps {
   function: lambda.IFunction;
 }
 
 export class StepFunctionRetriesStack extends cdk.Stack {
-  private func: lambda.IFunction
+  private retryProps : cdk.aws_stepfunctions.RetryProps
   constructor(scope: Construct, id: string, props: StepFunctionRetriesStackProps) {
     super(scope, id, props);
 
@@ -20,7 +17,7 @@ export class StepFunctionRetriesStack extends cdk.Stack {
     new sf.StateMachine(this, 'Retry', {
       definition: new tasks.LambdaInvoke(this, "trycatchfunction", {
         lambdaFunction: func,
-      }).next(new sf.Succeed(this, "End"))
+      }).addRetry(this.retryProps).next(new sf.Succeed(this, "End"))
     });
 
   }
